48620309b49734cd09695753a44d13c89b43f091
남민혁 <minhyuk.nam@outlook.com>
2020-09-30 15:41:34 +0000

    bug fixed

--- proj0-2/testlib.h
+++ proj0-2/testlib.h
@@ -5,6 +5,9 @@
 #include "hash.h"
 #include "bitmap.h"
 
+#define MAX_INPUT_SIZE 500
+#define MAX_REQUEST_TOKEN 20
+
 struct request{
 	int cnt;
 	char* token[MAX_REQUEST_TOKEN];
@@ -17,7 +20,7 @@ typedef struct new_data{
 	int exist;
 };
 
-void create(struct request req, new_data* new, int new_cnt);
-void dumpdata(struct request req, new_data* new, int new_cnt);
-void delete(struct request req, new_data* new, int new_cnt);
-void delete_all(new_data* new, int new_cnt);
+void create(struct request req, struct new_data* new, int new_cnt);
+void dumpdata(struct request req, struct new_data* new, int new_cnt);
+void delete(struct request req, struct new_data* new, int new_cnt);
+void delete_all(struct new_data* new, int new_cnt);
